c1cd71b48f7e4af5c97085db1cf9d5570d42908c
soleneprimo <160655587+soleneprimo@users.noreply.github.com>
2024-08-13 16:39:18 -0300

    criamos a pergunta

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-pricipal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector("caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector("texto-resultado");
+
+const perguntas =[
+{
+    enunciado: "pergunta 1",
+    alternativa:[
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "pergunta 2"
+    alternativa:[
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+];
