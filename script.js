c37c8d268bea211ae17a9e681a300e1dcc5656a9
soleneprimo <160655587+soleneprimo@users.noreply.github.com>
2024-08-20 16:24:44 -0300

    criação da historia

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ const textoResultado = document.querySelector("texto-resultado");
 
 const perguntas =[
 {
+    enunciado: "Você está vivenciando uma enchente, o que você faria?",
+    alternativa:[
+        "Continuava em casa",
+        "Saia a procura de ajuda"
+    ]
+},
+{
+    enunciado: "Você escolheu ficar em casa, mas a aguá começou à invadir a sua casa. Qual decisão você tomaria?"
+    alternativa:[
+        "Subir no telhado e esperar ajuda",
+        "Sair nadando"
+    ]
+},
+{
+    enunciado: "Você escolheu subir no telhado e esperar alguém lhe socorrer. Dias se passaram e você continuou no telhado com frio, embaixo de chuva, passando fome e sede. Você tem tuas escolhas:",
+    alternativa:[
+        "Desistir de viver e entrar dentro da aguá para morrer afogado.",
+        "Continuar esperando por ajuda."
+    ]
+},
+{
     enunciado: "pergunta 1",
     alternativa:[
         "Alternativa 1",
@@ -13,10 +34,18 @@ const perguntas =[
     ]
 },
 {
-    enunciado: "pergunta 2"
+    enunciado: "pergunta 1",
     alternativa:[
         "Alternativa 1",
         "Alternativa 2"
     ]
-}
+},
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas. textContent = perguntaAtual.enunciado
+}
